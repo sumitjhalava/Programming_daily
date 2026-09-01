4f8321f440f3c730c816c9cb386126fef28b74c0
Sumit Jhalava <161217901+sumitjhalava@users.noreply.github.com>
2026-09-01 22:47:12 +0530

    01-09-2026

---
+++ Pattern/Pattern12.java
@@ -0,0 +1,23 @@
+class Pattern12
+{
+    public static void main(String[] args)
+    {
+        int a = 0;
+        int n = 5 ;
+        for(int i = 1 ; i<=n ; i++)
+        {
+            for(int j = 1 ; j<= n-i;j++)
+            {
+            System.out.print("   ");
+            }
+            for(int k = 1 ; k<=i+a;k++)
+            {
+                System.out.print( " " + (char)(k+64) + " ");
+            }
+            a++;
+           System.out.println();
+
+        }
+       
+    }
+}
