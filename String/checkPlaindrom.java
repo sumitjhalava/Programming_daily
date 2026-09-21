403f7807e74a11fe7e6be48760525eb5e5aba62f
Sumit Jhalava <161217901+sumitjhalava@users.noreply.github.com>
2026-09-21 21:19:50 +0530

    21-09-2026

--- String/checkPlaindrom.java
+++ String/checkPlaindrom.java
@@ -1,7 +1,23 @@
 class checkPlaindrom
 {
+    static boolean checkPlaindrom(String s)
+    {
+        char [] a = s.toCharArray();
+        int i = 0 ; int j = a.length-1;
+        while(i<j)
+        {
+            if(a[i]!=a[j])
+            return false;
+            i++; j--;
+        }
+        return true;
+    }   
     public static void main(String[] args)
     {
-        
+        String s = "1112";
+        if(checkPlaindrom(s))
+        System.out.println("String is Plandrom");
+        else
+        System.out.println("String is not plandrom");
     }
 }
