e73f199ab2f24a2062845eec309fc94b8c69f53a
Sumit Jhalava <161217901+sumitjhalava@users.noreply.github.com>
2026-09-26 22:42:23 +0530

    26-02-2026

---
+++ ReverseEntierString.java
@@ -0,0 +1,12 @@
+class ReverseEntierString
+{
+    public static void main(String[] args) 
+	{
+		String s = "Ram is Good";
+		String [] a = s.split(" ");
+		for(int i =a.length-1; i>=0 ; i--) 
+		{
+			System.out.print(a[i] + " ");
+		}
+	}
+}
